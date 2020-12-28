d9ef7bf994dfb4e2aa81ae1e1ee784a5fec94b81
Sidm9 <siddharthm1999@outlook.com>
2020-12-28 09:17:51 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Sidebar />
+    <div class="navBoxes">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
-
+<script>
+import Sidebar from "./views/Sidebar.vue";
+export default {
+  components: {
+    Sidebar
+  }
+};
+</script>
 <style>
+* {
+  margin: 0px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  border-style: double;
+  width: 100%;
+  padding: 10px;
 }
 
 #nav {
-  padding: 30px;
+  border: black 1px solid;
+  /* padding: 30px; */
+  width: 30%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 #nav a {
@@ -28,5 +45,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.navBoxes {
+  width: 70%;
+  border: black 1px solid;
 }
 </style>
---
+++ src/views/Sidebar.vue
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div id="nav">
+    <router-link to="/">About</router-link>
+    <router-link to="/about">Projects</router-link>
+    <router-link to="/contacts">Contact</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
+
+<style></style>
