5e55aefcae2d023fb406870ca34fbad01b133873
Sidm9 <siddharthm1999@outlook.com>
2021-03-11 14:46:41 +0530

    Res Fixed

--- src/views/Sidebar.vue
+++ src/views/Sidebar.vue
@@ -62,9 +62,9 @@ export default {
   methods: {
     loadResume: function() {
       // https://drive.google.com/file/d/1cQYzoW-hFkytMArvQH1uTQ2xq1pKpvwY/view?usp=sharing
-      window.open(
-        "https://drive.google.com/u/1/uc?id=1cQYzoW-hFkytMArvQH1uTQ2xq1pKpvwY&export=download"
-      );
+      // window.open(
+      //   LINK ON NEW RESUME
+      // );
     }
   }
 };
