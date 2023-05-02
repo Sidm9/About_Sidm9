2d2f20ab291d35c3f6846699dab0d4b810588c09
sidm9 <siddharthm1999@outlook.com>
2023-05-02 11:00:25 +0530

    Analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { inject } from "@vercel/analytics";
+
+
+// Analytics
+inject({
+  debug: false,
+  mode: "auto"
+});
 
 Vue.config.productionTip = false;
 
